c7f7f842c2a150f3e01a0d28480e0ccf693a2675
Razan Alshatti <83327@kfh.com>
2024-04-28 08:49:49 +0300

    the step task & employee controller task

--- WebApplication5/Controllers/BankController.cs
+++ WebApplication5/Controllers/BankController.cs
@@ -1,10 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using WebApplication5.Models;
 
 namespace WebApplication5.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/bank")]
     [ApiController]
     public class BankController : ControllerBase
     {
@@ -15,15 +14,36 @@ namespace WebApplication5.Controllers
             _context = context;
         }
 
-        public List<BankBranch> getAll()
+        
+        [HttpGet]
+        public List<BankBranch> GetAll()
         {
             return _context.BankBranches.ToList();
         }
 
+        
+        [HttpGet("{id}")]
+        public ActionResult<BankBranch> GetBankBranch(int id)
+        {
+            var bankBranch = _context.BankBranches.FirstOrDefault(b => b.Id == id);
+
+            if (bankBranch == null)
+            {
+                return NotFound();
+            }
+
+            
+            bankBranch.Employees = _context.Employees.Where(e => e.BankBranchId == id).ToList();
+
+            return bankBranch;
+        }
+
+        
         [HttpPost]
         public IActionResult Add(AddBankRequest req)
         {
-            _context.BankBranches.Add(new BankBranch(){
+            _context.BankBranches.Add(new BankBranch()
+            {
                 location = req.location,
                 locationURL = req.locationURL,
                 branchManager = ""
---
+++ WebApplication5/Controllers/EmployeeController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    [Route("api/employee")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly BankContext _context;
+
+        public EmployeeController(BankContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployee(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return employee;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> Add(AddEmployeeRequest request)
+        {
+            var employee = new Employee
+            {
+                Name = request.Name,
+                civilId = request.civilId,
+                Position = request.position
+            };
+
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEmployee", new { id = employee.Id }, employee);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditEmployee(int id, AddEmployeeRequest request)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            employee.Name = request.Name;
+            employee.civilId = request.civilId;
+            employee.Position = request.position;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            return _context.Employees.Any(e => e.Id == id);
+        }
+    }
+}
